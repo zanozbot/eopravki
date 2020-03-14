c7edd29a29b47aca91b5f18682affe4dd337e990
Žan Ožbot <zan.ozbot@gmail.com>
2020-03-14 16:23:25 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let name;
+</script>
+
+<style lang="scss">
+  @import "./styles/global.scss";
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
