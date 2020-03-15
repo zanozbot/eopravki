b4d678a9cd65032ed211a23154aac9aa70a1b827
Žan Ožbot <zan.ozbot@gmail.com>
2020-03-15 16:06:45 +0100

    Created logo, favicon

--- src/Header.svelte
+++ src/Header.svelte
@@ -6,6 +6,9 @@
   .navbar {
     min-height: 4rem;
   }
+  .brand-name {
+    margin-right: 0.25rem;
+  }
 </style>
 
 <header>
@@ -15,7 +18,8 @@
     aria-label="main navigation">
     <div class="navbar-brand">
       <span class="navbar-item has-text-info is-size-5 has-text-weight-bold">
-        eDostava
+        <span class="brand-name">eDostava</span>
+        <img src="images/icon.png" alt="Logo eDostava" />
       </span>
     </div>
   </nav>
